b793b8607cfc92968ff9f79f9133c1cf3d1cfabf
imapolaris <sun_hui2011@sina.com>
2015-03-25 00:33:30 +0800

    创建项目
    创建项目--3/25/2015

---
+++ MongoDBHelper/DBHelper.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MongoDBHelper
+{
+    /// <summary>
+    /// 数据库操作 帮助类
+    /// </summary>
+    public class DBHelper
+    {
+    }
+}
